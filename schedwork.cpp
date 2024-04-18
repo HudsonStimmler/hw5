90328df9e5e941147411594afac270cae971083f
Hudson Stimmler <hstimmle@usc.edu>
2024-04-18 02:40:06 +0000

    final

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,27 +19,54 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
-// Add prototypes for any helper functions here
-
-
 // Add your implementation of schedule() and other helper functions here
-
-bool schedule(
+bool scheduleDay(
+    size_t daypos,
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
-    DailySchedule& sched
-)
+    DailySchedule& schedule,
+    vector<size_t>& sCount,
+    vector<Worker_T> currentDay,
+    size_t workerIndex,
+    size_t workersScheduled
+);
+
+
+bool schedule(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched) 
 {
     if(avail.size() == 0U){
         return false;
     }
     sched.clear();
-    // Add your code below
-
-
-
-
+    size_t k = avail[0].size(); 
+    sched.resize(avail.size());
+    vector<size_t> sCount(k, 0); 
+    vector<Worker_T> currentDay;
+    return scheduleDay(0, avail, dailyNeed, maxShifts, sched, sCount, currentDay, 0, 0);
 }
 
+
+bool scheduleDay(size_t daypos, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t>& sCount, vector<Worker_T> currentDay, size_t workerIndex, size_t workersScheduled) 
+{
+    if(daypos == avail.size()){
+        return true; 
+    }
+    if(workersScheduled == dailyNeed){
+        sched[daypos] = currentDay;
+        vector<Worker_T> a;
+        return scheduleDay(daypos + 1, avail, dailyNeed, maxShifts, sched, sCount, a, 0, 0);
+    }
+    for(size_t i = workerIndex; i < avail[daypos].size(); i++) {
+        if((avail[daypos][i] == true) && (sCount[i] < maxShifts)){
+            currentDay.push_back(i);
+            sCount[i] += 1;
+            if(scheduleDay(daypos, avail, dailyNeed, maxShifts, sched, sCount, currentDay, i + 1, workersScheduled + 1) == true){
+                return true;
+            }
+            sCount[i] -= 1;
+            currentDay.pop_back();
+        }
+    }
+    return false; 
+}
--- wordle.cpp
+++ wordle.cpp
@@ -14,15 +14,54 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void RecursiveFunc(const std::string& in, const std::string& floating, const std::set<string>& dict, const size_t NumUnknownChars, std::set<string>& answers, size_t position, const std::string& combo);
 
 // Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
-
+std::set<std::string> wordle(const std::string& in, const std::string& floating, const std::set<std::string>& dict){
+  // Add your code here
+  size_t NumUnknownChars = 0;
+  for(size_t i = 0; i < in.size(); i++){
+    if(in[i] == '-'){
+      NumUnknownChars += 1;
+    }
+  }
+  set<string> answers;
+  RecursiveFunc(in, floating, dict, NumUnknownChars, answers, 0, "");
+  return answers;  
 }
 
 // Define any helper functions here
+
+void RecursiveFunc(const std::string& in, const std::string& floating, const std::set<string>& dict, const size_t NumUnknownChars, set<string>& answers, size_t position, const string& combo){
+  if(position == in.size()){
+    if(dict.find(combo) != dict.end()){
+      answers.insert(combo);
+    }
+    return;
+  }
+
+  // add current character to word
+  if(in[position] != '-'){ 
+    RecursiveFunc(in, floating, dict, NumUnknownChars, answers, position+1, combo+in[position]);
+  }
+  else{
+    if(NumUnknownChars > floating.length()) {
+      for(char curr = 'a'; curr <= 'z'; curr++) {
+        string editFloat = floating;
+        size_t currPos = editFloat.find(curr);
+        if(currPos != string::npos){
+          editFloat = editFloat.substr(0, currPos) + floating.substr(currPos + 1);
+        }
+        RecursiveFunc(in, editFloat, dict, NumUnknownChars-1, answers, position+1, combo+curr);
+      }
+    } 
+    else{
+      for(size_t i = 0; i < floating.length(); i++){
+        string editFloat = floating;
+        char curr = editFloat[i];
+        editFloat = editFloat.substr(0, i) + floating.substr(i + 1);
+        RecursiveFunc(in, editFloat, dict, NumUnknownChars-1, answers, position+1, combo+curr);
+      }
+    }
+  }
+}
